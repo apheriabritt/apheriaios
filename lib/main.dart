0d5d8c0b5581e95d2bd71d185c65e9933e40e293
apheriabritt <britt@apheria.co.uk>
2020-03-01 21:46:07 +0000

    3.0.1: main > home with menu and about page no buttons work.

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,23 @@
+import 'package:apheriaios/home.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'apheria on ios',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home: MyHomePage(title: 'apheria on ios'),
+      theme: ThemeData(fontFamily: 'apheriafont'),
+      home: MyHomePage(),
+
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'apheria on ios',
-            ),
-            ]
-    )));
   }
 }
